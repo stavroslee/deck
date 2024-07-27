089311f786c1f277112a5936e727e03d323ac412
Stavros Lee <stavros.lee@gmail.com>
2024-07-26 19:15:16 -0700

    initial commit of boilerplate files

---
+++ deck/src/components/customComp.js
@@ -0,0 +1,7 @@
+// components/CustomComponent.js
+
+const CustomComponent = ({ prop1 }) => {
+  return <div>This is a custom component with prop: {prop1}</div>;
+};
+
+export default CustomComponent;
